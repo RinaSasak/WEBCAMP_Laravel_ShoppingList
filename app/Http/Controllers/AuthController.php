2b6608e95526724cfd0065d08092496d30e804ad
Rina Sasaki <rinasasaki@example.com>
2024-04-05 09:23:42 +0000

    認証と認可の実装まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
-//use Illuminate\Support\Facades\Auth;
-//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -27,32 +27,29 @@ class AuthController extends Controller
     {
         // validate済データの取得
         $datum = $request->validated();
-        var_dump($datum); exit;
+//var_dump($datum); exit;
 
         // 認証
-/*        if (Auth::attempt($datum) === false) {
+        if (Auth::attempt($datum) === false) {
             return back()
                    ->withInput() // 入力値の保持
                    ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
                    ;
         }
-
         //
         $request->session()->regenerate();
         return redirect()->intended('/shopping_list/list');
-*/
     }
-}
+    
     /**
      * ログアウト処理
      * 
      */
-/*     public function logout(Request $request)
+     public function logout(Request $request)
      {
          Auth::logout();
          $request->session()->regenerateToken(); // CSRトークンの生成
          $request->session()->regenerate(); // セッションIDの再生成
          return redirect(route('front.index'));
      }
-*/
-//}
+}
