26878ec48f4bc1794a10451b0f72bc1d654d0dbb
Rina Sasaki <rinasasaki@example.com>
2024-04-06 11:01:10 +0000

    課題提出_2回目　登録日の表示形式修正、完了ボタンと削除ボタンの間のカラムに余白挿入

--- database/migrations/2024_04_05_104837_create_completed_shopping_lists_table.php
+++ database/migrations/2024_04_05_104837_create_completed_shopping_lists_table.php
@@ -18,7 +18,7 @@ class CreateCompletedShoppingListsTable extends Migration
             $table->string('name', 255)->comment('「買うもの」名');
             $table->unsignedBigInteger('user_id')->comment('このリストの所有者');
             $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
-            $table->dateTime('created_at')->useCurrent();
+            $table->date('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrent()->userCurrentOnUpdate();
             //$table->timestamps();
             $table->primary('id');
